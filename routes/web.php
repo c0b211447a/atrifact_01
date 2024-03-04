31552c29e460b7ca06cafc898268d2904452d1f7
c0b211447a <c0b211447a@edu.teu.ac.jp>
2024-03-04 20:54:59 +0900

    不要ファイルの削除

--- routes/web.php
+++ routes/web.php
@@ -63,21 +63,4 @@ Route::middleware('auth')->group(function(){
     });
 });
 
-// Route::get('/cloths', [ClothController::class, 'index']);
-// Route::get('/cloths/categories/{select_category}', [CategoryController::class, 'showCategories'])->name('showCategories');
-// Route::get('/cloths/colors', [ClothController::class, 'showColors'])->name('showColors');
-// Route::get('/cloths/colors/{select_color}', [ColorController::class, 'showColors'])->name('showSelectColors');
-// Route::get('/cloths/items', [ClothController::class, 'showItems'])->name('showItems');
-// Route::post('/cloths/items', [ClothController::class, 'store_item'])->name('store_item');
-// Route::get('/cloths/items/add_items', [ClothController::class, 'add_item'])->name('add_item');
-// Route::delete('/cloths/items/{item_id}', [ClothController::class, 'delete_item'])->name('delete_item');
-// Route::post('/cloths/items/{item_id}', [ClothController::class, 'update_item'])->name('update_item');
-// Route::get('/cloths/items/{item_id}/edit', [ClothController::class, 'edit_item'])->name('edit_item');
-// Route::get('/cloths/patterns', [PatternsController::class, 'showPatterns'])->name('showPatterns');
-// Route::get('/cloths/patterns/item/{item_id}', [PatternsController::class, 'showItemInPatterns'])->name('showItemInPatterns');
-// Route::delete('/cloths/patterns/delete_patterns/{patterns_id}', [PatternsController::class, 'delete_patterns'])->name('delete_patterns');
-// Route::get('/cloths/patterns/edit_patterns/{patterns_id}', [PatternsController::class, 'edit_patterns'])->name('edit_patterns');
-// Route::put('/cloths/patterns/update_patterns', [PatternsController::class, 'update_patterns'])->name('update_patterns');
-// Route::get('/cloths/patterns/add_patterns', [PatternsController::class, 'add_pattern'])->name('add_pattern');
-// Route::post('/cloths/patterns/add_patterns/store', [PatternsController::class, 'store_pattern'])->name('store_pattern');
 require __DIR__.'/auth.php';
